1b37033b013c7803a7f8d8b12e4d39c90dcbd4d5
lefilou-axb <lucas.haegel@gmail.com>
2026-01-08 09:50:46 +0100

    Question 7, debut question 8

--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,11 +1,14 @@
 package test_fonctionnel;
 
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix",8);
+		Romain minus = new Romain("Minus", 6);
 		System.out.println(asterix.getNom());
-		asterix.parler("coucou");
+		asterix.parler("Bonjour à tous");
+		minus.parler("UN GAU... UN GAUGAU...");
 	}
 }
